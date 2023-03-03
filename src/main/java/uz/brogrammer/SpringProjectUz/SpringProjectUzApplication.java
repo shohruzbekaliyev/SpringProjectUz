f27e5f502c8b930dc25413699b31d0bc754f8d8e
shohruzbekaliyev <chessmasteruzbek007@gmail.com>
2023-03-03 02:46:41 -0800

    Initial commit

---
+++ src/main/java/uz/brogrammer/SpringProjectUz/SpringProjectUzApplication.java
@@ -0,0 +1,13 @@
+package uz.brogrammer.SpringProjectUz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectUzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectUzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/brogrammer/SpringProjectUz/SpringProjectUzApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.brogrammer.SpringProjectUz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectUzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
